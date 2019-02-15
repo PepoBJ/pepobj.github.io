18cc4ebeadb7de3a05dc2729a6c7e7247302ad07
Robert HC <bj112143@gmail.com>
2019-02-15 18:16:39 -0500

    Refactor: agregando redirección a https://roberthuaman.com

--- dev/es6/codigos.js
+++ dev/es6/codigos.js
@@ -1,3 +1,8 @@
 'use strict';
 
 new GitHubCalendar(".calendar", "pepobj", {summary_text: "", global_stats: false, responsive : true});
+
+if(location.href.indexOf('roberthuaman.com') === -1 && location.href.indexOf('http://localhost:') === -1)
+{
+    location.href = 'https://roberthuaman.com/';
+}
--- dist/js/codigos.js
+++ dist/js/codigos.js
@@ -1,2 +1,2 @@
-"use strict";new GitHubCalendar(".calendar","pepobj",{summary_text:"",global_stats:!1,responsive:!0});
+"use strict";new GitHubCalendar(".calendar","pepobj",{summary_text:"",global_stats:!1,responsive:!0}),-1===location.href.indexOf("roberthuaman.com")&&-1===location.href.indexOf("http://localhost:")&&(location.href="https://roberthuaman.com/");
 //# sourceMappingURL=codigos.js.map
