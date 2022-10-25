ea7b54e690354a4c21a15ee7fcbdf9bdfa66992c
Robert HC <bj112143@gmail.com>
2022-10-25 01:34:37 -0500

    Refactor: clean repo

--- dev/es6/codigos.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-new GitHubCalendar(".calendar", "pepobj", {summary_text: "", global_stats: false, responsive : true});
-
-if(location.href.indexOf('roberthuaman.com') === -1 && location.href.indexOf('http://localhost:') === -1)
-{
-    location.href = 'https://roberthuaman.com/';
-}
